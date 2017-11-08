1eff78841bc06e07f93ecb2fac5132e0d149de13
Richard Bruskiewich <richard@starinformatics.com>
2017-11-08 07:35:41 -0800

    Trap errors and fail gracefully in StringListInitializer

--- server/src/main/java/bio/knowledge/server/impl/StringListDeserializer.java
+++ server/src/main/java/bio/knowledge/server/impl/StringListDeserializer.java
@@ -1,7 +1,11 @@
 package bio.knowledge.server.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -18,15 +22,39 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  */
 public class StringListDeserializer extends JsonDeserializer<List<String>> {
-
+	
+	private static Logger _logger = LoggerFactory.getLogger(StringListDeserializer.class);	
+	
 	@Override
 	public List<String> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
 		
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
-		@SuppressWarnings("unchecked")
-		List<Object> values = mapper.readValue(p, List.class);
-		List<String> strings = Util.map(Object::toString, values);
+
+		List<String> strings = null;
+		try {
+			
+			@SuppressWarnings("unchecked")
+			List<Object> values = mapper.readValue(p, List.class);
+			
+			if(values!=null && values.size()>0)
+				strings = Util.map(Object::toString, values);
+			
+		} catch (NullPointerException npe) {
+			
+			_logger.error("StringListDeserializer() ERROR: Null pointer encountered?");
+			
+		} catch (IOException ioe) {
+			
+			_logger.error("StringListDeserializer() ERROR: IOException encountered? "+ioe.getMessage());
+			throw new IOException(ioe);
+			
+		} finally {
+			
+			if(strings==null)
+				strings = new ArrayList<String>(); // return an empty list
+		}
+		
 		return strings;
 	}
 
